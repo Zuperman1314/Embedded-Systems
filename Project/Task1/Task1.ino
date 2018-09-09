3e4e0ab0893850f95b01afcd65fc6263575625d5
Amir <amir_rajabifar@yahoo.com.au>
2018-09-09 23:45:35 +0800

    Cycle between program processes

--- Project/Task1/Task1.ino
+++ Project/Task1/Task1.ino
@@ -16,28 +16,78 @@
 */
 
 #include "Task1.h"
+#define MAX_PROCESSES 3
+
+volatile int flag = LOW;
+int counter = 0; // A counter for number of pprocesses
 
 void setup()
 {
-	
-
+	pinMode(PUSH2, INPUT_PULLUP); //set the push button 2 as input with the pull up resistor
+	pinMode(LED1, OUTPUT);
+	attachInterrupt(PUSH2, count, FALLING); //Interrupt id fired whenever button is pressed	
+	Serial.begin(9600);
+	Serial.println("Initialized");
 }
 
 void loop()
 {
-	switch (/*counter*/) 
+	if (flag)
 	{
-	    case : 1
+		if (counter == MAX_PROCESSES)
+		{
+			counter = 1;
+		}
+		else
+		{
+			counter++;
+		}
+		flag = LOW;
+	}
+	#ifdef DEBUG
+		Serial.println(counter);
+	#endif
+	switch (counter) 
+	{
+	    case 1:
 	      // Name -> LCE
+	    	#ifdef DEBUG
+		    	Serial.print(counter);
+		    	Serial.print('\t');
+		    	Serial.println("Mode 1");
+	    	#endif
 	      break;
-	    case : 2
+	    case 2:
 	      // Temp -> LCD
 	      // Temp (trigger) -> servo
-	    case : 3
+	    	#ifdef DEBUG
+		    	Serial.print(counter);
+		    	Serial.print('\t');
+		    	Serial.println("Mode 2");
+	    	#endif
+	    	break;
+	    case 3:
 	      // acce -> LCD
 	      // acce -> LEDs
+	    	#ifdef DEBUG
+		    	Serial.print(counter);
+		    	Serial.print('\t');
+		    	Serial.println("Mode 3");
+	    	#endif
 	      break;
 	    default:
 	      // ERROR
+	      break;
 	}
+}
+
+void count()
+{
+	static unsigned long last_int_time = 0;
+	unsigned long int_time = millis();
+	if (int_time - last_int_time > 200)
+	{
+		flag = HIGH;
+	}
+	last_int_time = int_time;
 }
